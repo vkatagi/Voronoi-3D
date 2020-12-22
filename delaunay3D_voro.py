61e37e1dd68184041d9993425a3a869e0346d4b4
katagis <fox.dealos@gmail.com>
2020-12-22 20:25:21 +0200

    Add tetrahedrization and 3d line preview

--- delaunay3D_voro.py
+++ delaunay3D_voro.py
@@ -37,94 +37,91 @@ class Tetrahedron:
         ])
         self.circum_radius = np.linalg.norm(self.vert[0].p - self.circum)
 
-    def sharesFaceWith(self, other):
-        # TODO:
-        count = 0
-        for vert in self.vert:
-            if vert in other.vert:
-                count+=1
-        return count == 2
-
     def vertexInCircum(self, vert):
         return np.linalg.norm(vert.p - self.circum) < self.circum_radius
+    
+    def sharesFaceWith(self, other):
+        return len(set(self.vert).intersection(other.vert)) >= 3
 
     # Returns neighboring triangles set. (All triangles that share an edge with this one)
-    def neighbors(self):
-        # TODO: 3d
-        # PERF: can be optimized
-        result = set()
-        for vtx in self.vert:
-            for adj in vtx.adj:
-                if adj is not self and self.sharesEdgeWith(adj):
-                    result.add(adj)
-        return result;
+    # def neighbors(self):
+    #     # TODO: 3d
+    #     # PERF: can be optimized
+    #     result = set()
+    #     for vtx in self.vert:
+    #         for adj in vtx.adj:
+    #             if adj is not self and self.sharesEdgeWith(adj):
+    #                 result.add(adj)
+    #     return result;
 
-class Edge:
-    def __init__(self, p0, p1):
-        self.vert = [p0, p1]
+class Face:
+    def __init__(self, p0, p1, p2):
+        self.vert = [p0, p1, p2]
 
     def __eq__(self, other):
-        # Assumes p0 != p1 always.
-        if self.vert[0] == other.vert[0] or self.vert[0] == other.vert[1]:
-            if self.vert[1] == other.vert[0] or self.vert[1] == other.vert[1]:
-                return True
-        return False
-
-
-
-def findPolygon(badTriangles):
+        return len(set(self.vert).intersection(other.vert)) == 3
+        
+def findPolyhedron(badTetras):
     # PERF: can be optimized a lot
-    all_edges = []
-    for tri in badTriangles:
-        all_edges.append(Edge(tri.vert[0], tri.vert[1]))
-        all_edges.append(Edge(tri.vert[1], tri.vert[2]))
-        all_edges.append(Edge(tri.vert[2], tri.vert[0]))
+    all_faces = []
+    for poly in badTetras:
+        all_faces.append(Face(poly.vert[0], poly.vert[1], poly.vert[2]))
+        all_faces.append(Face(poly.vert[1], poly.vert[2], poly.vert[3]))
+        all_faces.append(Face(poly.vert[2], poly.vert[3], poly.vert[0]))
+        all_faces.append(Face(poly.vert[3], poly.vert[0], poly.vert[1]))
     
-    edges = []
-    for edge in all_edges:
+    faces = []
+    for face in all_faces:
         found = 0
-        for tested in all_edges:
-            if tested == edge:
+        for tested in all_faces:
+            if tested == face:
                 found += 1
                 if found > 1:
                     break
         if found == 1:
-            edges.append(edge)
+            faces.append(face)
 
-    return edges
-
+    return faces
 
 def triangulate(points):
     verts = []
     for p in points:
         verts.append(Vertex(p))
     
-    # super triangle
-    verts.append(Vertex([1000, 0]))
-    verts.append(Vertex([0, 1000]))
-    verts.append(Vertex([0, 0]))
+    # super polyhedron
+    d = 100
+    verts.append(Vertex([d, 0, 0]))
+    verts.append(Vertex([0, d, 0]))
+    verts.append(Vertex([0, 0, d]))
+    verts.append(Vertex([0, 0, 0]))
 
-    triangles = set()
-    triangles.add(Triangle(verts[-3], verts[-2], verts[-1]))
+    delaunay = set()
+    delaunay.add(Tetrahedron(verts[-4], verts[-3], verts[-2], verts[-1]))
 
     for vtx in verts:
-        # Find bad triangles, (all the triangles that vtx is inside of the circumcircle)
-        bad = set(filter(lambda tri: (tri.vertexInCircum(vtx)), triangles))
+        # Find bad tetrahedrons
+        bad = set(filter(lambda tetr: (tetr.vertexInCircum(vtx)), delaunay))
 
-        # Find polygonal hole from bad triangles
-        polygon = findPolygon(bad)
+        # Find polygonal hole from bad tetrahedrons
+        polyhedron = findPolyhedron(bad)
 
-        # Remove bad triangles from triangulation
-        for tri in bad:
-            for triVtx in tri.vert:
-                triVtx.adj.remove(tri)
-            triangles.remove(tri)
+        # Remove bad tetrahedrons from triangulation
+        for tetra in bad:
+            for tetraVtx in tetra.vert:
+                tetraVtx.adj.remove(tetra)
+            delaunay.remove(tetra)
 
-        for edge in polygon:
-            # assumes all edges are valid (non points)
-            triangles.add(Triangle(vtx, edge.vert[0], edge.vert[1]))
+        for face in polyhedron:
+            delaunay.add(Tetrahedron(face.vert[0], face.vert[1], face.vert[2], vtx))
+
+    superTetras = set()
+    for vert in verts[-4:]:
+        superTetras = superTetras.union(vert.adj)
     
-    return triangles
+    for tetra in superTetras:
+        delaunay.remove(tetra)
+
+    return delaunay
         
 def voronoi(triangulation):
     edges = []
@@ -133,39 +130,54 @@ def voronoi(triangulation):
             edges.append(Edge(tri.circum, neighbor.circum))
     return edges 
 
-
-
-##
-## Driver
-##
-
-# imported later. these are only used for plotting & drawing
+# imports. these are only used for plotting & drawing
 import scipy as sp
 import scipy.spatial
 import matplotlib.pyplot as plt
 import matplotlib.collections
 from mpl_toolkits.mplot3d import Axes3D 
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from itertools import chain
 
+##
+## Driver
+##
 
 def main():
-    tetr = Tetrahedron(
-        Vertex([0, 0, 0]), 
-        Vertex([2, 0, 0]), 
-        Vertex([1, 1, 0]), 
-        Vertex([1, 0, 1])
-    )
 
+    # tetr = Tetrahedron(
+    #     Vertex([0, 0, 0]), 
+    #     Vertex([2, 0, 0]), 
+    #     Vertex([1, 1, 0]), 
+    #     Vertex([1, 0, 1])
+    # )
 
-    return
-    points = np.array([[0, 0, 0], [3.2, 1.4, 0.1] , [3.1, 5, 2], [2.7, 4.1, 1], [2.9, 1, 1.1], [1, 3, 3.4], [5, 5, 1], [4.6, 2.1, 4]])
+    points = np.array([[0.1, 0.1, 0.1], [3.2, 1.4, 0.1] , [3.1, 5, 2], [2.7, 4.1, 1], [2.9, 1, 1.1], [1, 3, 3.4], [5, 5, 1], [4.6, 2.1, 4]])
+
+    delu = triangulate(points)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    
-    print(points[:,0]);
-
     ax.scatter(points[:,0], points[:,1], points[:,2])
+
+    lines = []
+    for tetra in delu:
+        lines.append((tetra.vert[0].p, tetra.vert[1].p))
+        lines.append((tetra.vert[0].p, tetra.vert[2].p))
+        lines.append((tetra.vert[0].p, tetra.vert[3].p))
+        lines.append((tetra.vert[1].p, tetra.vert[3].p))
+        lines.append((tetra.vert[1].p, tetra.vert[2].p))
+        lines.append((tetra.vert[2].p, tetra.vert[3].p))
+        
+    for a, b in lines:
+        ax.plot([a[0], b[0]], [a[1], b[1]], [a[2], b[2]])
+
     plt.show()
+
+    return
+
+    
+
 
 
     # triangles = triangulate(points)
